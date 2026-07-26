782e4f7349ed21b5c9753f873ae1697ce15f3b22
Robson-Haro <rramosrh@gmail.com>
2026-07-26 10:25:53 -0300

    Define Talent Manager como página inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Portal from './Portal'
 import './i18n'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Portal />
   </React.StrictMode>
 )
